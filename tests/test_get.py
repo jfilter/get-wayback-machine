bff172f21eb67f48fd54c7420e212c881e93dac6
Johannes Filter <hi@jfilter.de>
2018-12-14 21:43:50 +0100

    add test for fail

--- tests/test_get.py
+++ tests/test_get.py
@@ -5,3 +5,9 @@ def test_success():
     res = get_wayback_machine.get(
         'https://tvline.com/2016/04/18/stana-katic-leaving-castle-season-9-beckett-dies/')
     assert(res.status_code == 200)
+
+
+def test_fail():
+    res = get_wayback_machine.get(
+        'https://tvlineXXXX.com/2016/04/18/stana-katic-leaving-castle-season-9-beckett-dies/')
+    assert(res is None)
